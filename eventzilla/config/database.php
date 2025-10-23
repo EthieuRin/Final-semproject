0c9af5e63c4b87c9947838cee97120cdc6bdf84c
Wesley Kyarua <wesley.kyarua@strathmore.edu>
2025-10-23 12:48:14 +0300

    Database.php update

--- eventzilla/config/database.php
+++ eventzilla/config/database.php
@@ -3,27 +3,23 @@ class Database {
     private $db_path;
     private $conn;
 
-<<<<<<< HEA
-    
-=======
     public function __construct() {
         $this->db_path = __DIR__ . '/../eventzilla.db';
     }
 
->>>>>>> 469b3a352a5281954ad279098dbed81dbf6e39e8
     public function getConnection() {
         $this->conn = null;
-        
-        
+
         try {
             $this->conn = new PDO("sqlite:" . $this->db_path);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             // Enable foreign key support
             $this->conn->exec("PRAGMA foreign_keys = ON");
-        } catch(PDOException $exception) {
+        } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
-        
+
         return $this->conn;
     }
 }
